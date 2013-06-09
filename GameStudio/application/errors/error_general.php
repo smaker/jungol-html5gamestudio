b7c10e9fc01dee5955320edd39c5d8a9879bca30
Smaker <dowon2308@gmail.com>
2013-06-09 13:44:04 +0900

    #5 일반 에러 페이지 리펙토링

--- GameStudio/application/errors/error_general.php
+++ GameStudio/application/errors/error_general.php
@@ -1,62 +1,31 @@
+<?php
+$base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http');
+$base_url .= '://' . $_SERVER['HTTP_HOST'];
+$base_url .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Error</title>
-<style type="text/css">
-
-::selection{ background-color: #E13300; color: white; }
-::moz-selection{ background-color: #E13300; color: white; }
-::webkit-selection{ background-color: #E13300; color: white; }
-
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	-webkit-box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+	<title>Error</title>
+	<link rel="stylesheet" href="<?php echo $base_url; ?>application/third_party/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo $base_url; ?>application/third_party/bootstrap/css/bootstrap-responsive.min.css">
 </head>
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+	<div class="container">
+		<div class="hero-unit">
+		  <h1><?php echo $heading; ?></h1>
+		  <p><?php echo $message; ?></p>
+		  <p>
+
+		    <a href="<?php if(isset($_SERVER['HTTP_REFERER'])) { echo $_SERVER['HTTP_REFERER']; } else { ?>#<?php } ?>" onclick="history.back();" class="btn btn-large">
+		      새로고침
+		    </a>
+		    <a href="<?php if(isset($_SERVER['HTTP_REFERER'])) { echo $_SERVER['HTTP_REFERER']; } else { ?>#<?php } ?>" onclick="history.back();" class="btn btn-primary btn-large">
+		      돌아가기
+		    </a>
+		  </p>
 	</div>
 </body>
 </html>
